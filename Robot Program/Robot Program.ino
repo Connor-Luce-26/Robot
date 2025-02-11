e811cc956f46f6c27697aa7783fda25e02aec3a7
Connor-Luce <lucerc@rose-hulman.edu>
2025-02-10 22:18:10 -0500

    Revert "Add IMU class for integrated sensor management; implement VectorIntegral for velocity, position, and angle calculations"
    This reverts commit 24b02016e7ab5de074b58973aa617a1ddea3956b.

--- Robot Program/Robot Program.ino
+++ Robot Program/Robot Program.ino
@@ -1,15 +1,26 @@
 #include <Arduino.h>
-#include "IMU.hpp"
-IMU imu = IMU(ACCELEROMETER_FULL_SCALE_RANGE_0, GYROSCOPE_FULL_SCALE_RANGE_0);
+//#include "HCSR04.hpp"
+#include "MPU6050.hpp"
+#include "Integral.hpp"
+MPU6050 mpu(ACCELEROMETER_FULL_SCALE_RANGE_0, GYROSCOPE_FULL_SCALE_RANGE_0);
+Integral velocity;
+Integral position;
+int counter = 0;
 void setup() 
 {
     Serial.begin(115200);
-    imu.setup();
-	Serial.println(imu.getCalibrationString());
+    mpu.setup();
+	Serial.print(mpu.getCalibrationString());
 }
+
 void loop() 
 {	
-	imu.update();
-	Serial.println(imu.getVelocity().toString());
+	Serial.print("X Accelerometer: ");
+	Serial.println(mpu.getXAccelerometer());
+	// position.update(velocity.update(fir.update(mpu.getXAccelerometer())));
+	// Serial.print("Position: ");
+	// Serial.println(position.getValue());
+	// Serial.print("Velocity: ");
+	// Serial.println(velocity.getValue());
 	delay(1000);
 }
--- Robot Program/Vector.hpp
+++ Robot Program/Vector.hpp
@@ -2,36 +2,36 @@
 #include "ArrayList.hpp"
 #include "String.h"
 #include "UTF8Symbols.hpp"
-// Class representing a generic Vector
+// Class representing a generic ArrayList
 class Vector
 {
 private:
-	// ArrayList to store the values of the Vector
+	// Vector to store the values of the ArrayList
 	ArrayList<double> value;
-	// Shape of the Vector
+	// Shape of the ArrayList
 	String shape;
 
 public:
-	// Constructor that initializes the Vector with a given ArrayList of values and a given shape
+	// Constructor that initializes the ArrayList with a given ArrayList of values and a given shape
 	Vector(ArrayList<double> value, String shape)
 	{
 		this->value = value;
 		this->shape = shape;
 	}
-	// Constructor that initializes the Vector with a given shape and size, and fills it with a given value
+	// Constructor that initializes the ArrayList with a given shape and size, and fills it with a given value
 	Vector(int size, double value, String shape)
 	{
 		this->shape = shape;
 		for (int i = 0; i < size; i++)
 		{
-			this->value.add(value);
+			this->value.push_back(value);
 		}
 	}
-	// Constructor that initializes the Vector with a given size and fills it with zeros
+	// Constructor that initializes the ArrayList with a given size and fills it with zeros
 	Vector(int size, String shape) : Vector(size, 0.0, shape)
 	{
 	}
-	// Default constructor that initializes an empty Vector
+	// Default constructor that initializes an empty ArrayList
 	Vector()
 	{
 		this->value = ArrayList<double>();
@@ -41,78 +41,78 @@ public:
 	~Vector()
 	{
 	}
-	// Get the size of the Vector
-	int getSize()
+	// Get the size of the ArrayList
+	int size()
 	{
-		return this->value.getSize();
+		return this->value.size();
 	}
-	// Get the value of the Vector
+	// Get the value of the ArrayList
 	ArrayList<double> getValue()
 	{
 		return this->value;
 	}
-	// Get the value of the Vector at a given index
+	// Get the value of the ArrayList at a given index
 	double getValue(int index)
 	{
 		return this->value[index];
 	}
-	// Get the shape of the Vector
+	// Get the shape of the ArrayList
 	String getShape()
 	{
 		return this->shape;
 	}
-	// Set the value of the Vector
+	// Set the value of the ArrayList
 	void setValue(ArrayList<double> value)
 	{
 		this->value = value;
 	}
-	// Set the value of the Vector at a given index
+	// Set the value of the ArrayList at a given index
 	void setValue(int index, double value)
 	{
 		this->value[index] = value;
 	}
-	// Set the shape of the Vector
+	// Set the shape of the ArrayList
 	void setShape(String shape)
 	{
 		this->shape = shape;
 	}
-	// Method to overload the + operator to add two Vectors
+	// Method to overload the + operator to add two ArrayLists
 	Vector operator+(Vector other)
 	{
 		this->ensureEqualSize(other);
 		this->ensureEqualShape(other);
 		ArrayList<double> result;
-		for (int i = 0; i < this->getSize(); i++)
+		for (int i = 0; i < this->size(); i++)
 		{
-			result.add(this->getValue(i) + other.getValue(i));
+			result.push_back(this->getValue(i) + other.getValue(i));
 		}
 		return Vector(result, this->shape);
 	}
-	// Method to overload the * operator to multiply a Vector by a scalar
+	// Method to overload the * operator to multiply a ArrayList by a scalar
 	Vector operator*(double scalar)
 	{
 		ArrayList<double> result;
-		for (int i = 0; i < this->getSize(); i++)
+		for (int i = 0; i < this->size(); i++)
 		{
-			result.add(this->getValue(i) * scalar);
+			result.push_back(this->getValue(i) * scalar);
 		}
 		return Vector(result, this->shape);
 	}
-	// Method to overload the - operator to subtract two Vectors
+	// Method to overload the - operator to subtract two ArrayLists
 	Vector operator-(Vector other)
 	{
 		return *this + other * -1.0;
 	}
-	// Method to overload the / operator to divide a Vector by a scalar
+	// Method to overload the / operator to divide a ArrayList by a scalar
 	Vector operator/(double scalar)
 	{
 		return *this * (1.0 / scalar);
 	}
-	// Method to overload the == operator to compare two Vectors
+	// Method to overload the == operator to compare two ArrayLists
 	bool operator==(Vector other)
 	{
 		this->ensureEqualSize(other);
-		for (int i = 0; i < this->getSize(); i++)
+		for (int i = 0; i < this->size(); i++)
 		{
 			if (this->getValue(i) != other.getValue(i))
 			{
@@ -121,27 +121,27 @@ public:
 		}
 		return true;
 	}
-	// Method to calculate the dot product of two Vectors
+	// Method to calculate the dot product of two ArrayLists
 	double dotProduct(Vector other)
 	{
 		this->ensureEqualSize(other);
 		this->ensureEqualShape(other);
 		double result = 0.0;
-		for (int i = 0; i < this->getSize(); i++)
+		for (int i = 0; i < this->size(); i++)
 		{
 			result += this->getValue(i) * other.getValue(i);
 		}
 		return result;
 	}
-	// Method to throw an error if the Vectors are not the same size
+	// Method to throw an error if the ArrayLists are not the same size
 	void ensureEqualSize(Vector other)
 	{
-		if (this->getSize() != other.getSize())
+		if (this->size() != other.size())
 		{
 			Serial.println("Vectors must have the same size");
 		}
 	}
-	// Method to throw an error if the Vectors are not the same shape
+	// Method to throw an error if the ArrayLists are not the same shape
 	void ensureEqualShape(Vector other)
 	{
 		if (this->getShape() != other.getShape())
@@ -149,87 +149,87 @@ public:
 			Serial.println("Vectors must have the same shape");
 		}
 	}
-	// Method to find the String representation of a Vector element that is the longest
+	// Method to find the String representation of a ArrayList element that is the longest
 	int findLongestElementLength()
 	{
 		int result = 0;
-		for (int i = 0; i < this->getSize(); i++)
+		for (int i = 0; i < this->size(); i++)
 		{
-			if (result < toString(this->getValue(i)).getSize())
+			if (result < to_String(this->getValue(i)).size())
 			{
-				result = toString(this->getValue(i)).getSize();
+				result = to_String(this->getValue(i)).size();
 			}
 		}
 		return result;
 	}
-	// Method to get the String representation of the Vector
-	String toString()
+	// Method to print the ArrayList
+	void print()
 	{
 		int longestElementLength = this->findLongestElementLength();
 		if (this->shape == "column")
 		{
-			return this->columnVectorToString(longestElementLength);
+			this->printColumnVector(longestElementLength);
 		}
 		else if (this->shape == "row")
 		{
-			return this->rowVectorToString(longestElementLength);
+			this->printRowVector(longestElementLength);
 		}
 		else
 		{
-			return String("Invalid shape");
+			Serial.println("Invalid shape");
 		}
 	}
-	// Method to get the String representation of a row vector
-	String rowVectorToString(int longestElementLength)
-	{
-		String result = LEFT_SQUARE_BRACKET + " ";
-		int padding = 0;
-		String element = "";
-		for (int i = 0; i < Vector::getSize() - 1; i++)
-		{
-			element = toString(this->getValue(i));
-			padding = longestElementLength - element.getSize();
-			result += element + String(padding, ' ') + " ";
-		}
-		element = toString(this->getValue(this->getSize() - 1));
-		padding = longestElementLength - element.getSize();
-		result += element + String(padding, ' ') + " " + RIGHT_SQUARE_BRACKET + "\n";
-		return result;
-	}
-	// Method to get the String representation of a column vector
- 	String columnVectorToString(int longestElementLength)
-	{
-		String element = toString(this->getValue(0));
-		int padding = longestElementLength - element.getSize();
-		String result = LEFT_SQUARE_BRACKET_UPPER_CORNER + " " + element + String(padding, ' ') + " " + RIGHT_SQUARE_BRACKET_UPPER_CORNER + "\n";
-		for (int i = 1; i < Vector::getSize() - 1; i++)
-		{
-			element = toString(this->getValue(i));
-			padding = longestElementLength - element.getSize();
-			result += LEFT_SQUARE_BRACKET_EXTENSION + " " + element + String(padding, ' ') + " " + RIGHT_SQUARE_BRACKET_EXTENSION + "\n";
-		}
-		element = toString(this->getValue(this->getSize() - 1));
-		padding = longestElementLength - element.getSize();
-		result += LEFT_SQUARE_BRACKET_LOWER_CORNER + " " + element + String(padding, ' ') + " " + RIGHT_SQUARE_BRACKET_LOWER_CORNER + "\n";
-		return result;
-	}
-	// Method to get the String representation of the Vector
-	String toString(int longestElementLength)
+	// Method to print the ArrayList given the longest element length
+	void print(int longestElementLength)
 	{
 		if (this->shape == "column")
 		{
-			return this->columnVectorToString(longestElementLength);
+			this->printColumnVector(longestElementLength);
 		}
 		else if (this->shape == "row")
 		{
-			return this->rowVectorToString(longestElementLength);
+			this->printRowVector(longestElementLength);
 		}
 		else
 		{
-			return String("Invalid shape");
+			Serial.println("Invalid shape");
 		}
 	}
-	// Method to calculate the transpose of the Vector
+	// Method to print the row ArrayList
+	void printRowVector(int longestElementLength)
+	{
+		String result = LEFT_SQUARE_BRACKET + " ";
+	    int padding = 0;
+		String element = "";
+		for (int i = 0; i < Vector::size() - 1; i++)
+		{
+			element = to_String(this->getValue(i));
+			padding = longestElementLength - element.size();
+			result += element + String(padding, ' ') + " ";
+		}
+		element = to_String(this->getValue(this->size() - 1));
+		padding = longestElementLength - element.size();
+		result += element + String(padding, ' ') + " " + RIGHT_SQUARE_BRACKET + "\n";
+		Serial.print(result);
+	}
+	// Method to print the column ArrayList
+	void printColumnVector(int longestElementLength)
+	{
+		String element = to_String(this->getValue(0));
+		int padding = longestElementLength - element.size();
+		String result = LEFT_SQUARE_BRACKET_UPPER_CORNER + " " + element + String(padding, ' ') + " " + RIGHT_SQUARE_BRACKET_UPPER_CORNER + "\n";
+		for (int i = 1; i < Vector::size() - 1; i++)
+		{
+			element = to_String(this->getValue(i));
+			padding = longestElementLength - element.size();
+			result += LEFT_SQUARE_BRACKET_EXTENSION + " " + element + String(padding, ' ') + " " + RIGHT_SQUARE_BRACKET_EXTENSION + "\n";
+		}
+		element = to_String(this->getValue(this->size() - 1));
+		padding = longestElementLength - element.size();
+		result += LEFT_SQUARE_BRACKET_LOWER_CORNER + " " + element + String(padding, ' ') + " " + RIGHT_SQUARE_BRACKET_LOWER_CORNER + "\n";
+		Serial.print(result);
+	}
+	// Method to calculate the transpose of the ArrayList
 	Vector transpose()
 	{
 		if (this->shape == "column")
@@ -245,37 +245,37 @@ public:
 			Serial.println("Invalid shape");
 		}
 	}
-	// Method to calculate the p-norm of the Vector
+	// Method to calculate the p-norm of the ArrayList
 	double pNorm(unsigned int p)
 	{
 		double result = 0.0;
-		for (int i = 0; i < this->getSize(); i++)
+		for (int i = 0; i < this->size(); i++)
 		{
 			result += pow(abs(this->getValue(i)), p);
 		}
 		return pow(result, 1.0 / p);
 	}
-	// Method to calculate the infinity norm of the Vector
+	// Method to calculate the infinity norm of the ArrayList
 	double infinityNorm()
 	{
 		double result = 0.0;
-		for (int i = 0; i < this->getSize(); i++)
+		for (int i = 0; i < this->size(); i++)
 		{
 			result = max(result, abs(this->getValue(i)));
 		}
 		return result;
 	}
-	// Method to calculate the magnitude of the Vector
+	// Method to calculate the magnitude of the ArrayList
 	double magnitude()
 	{
 		return this->pNorm(2);
 	}
-	// Method to calculate the unit Vector
+	// Method to calculate the unit ArrayList
 	Vector unitVector()
 	{
 		return *this / this->magnitude();
 	}
-	// Method to overload the * operator to multiply a Vector by another Vector
+	// Method to overload the * operator to multiply a ArrayList by another ArrayList
 	double operator*(Vector other)
 	{
 		this->ensureEqualSize(other);
@@ -288,7 +288,7 @@ public:
 			Serial.println("Invalid shapes");
 		}
 	}
-	// Method to overload the != operator to compare two Vectors
+	// Method to overload the != operator to compare two ArrayLists
 	bool operator!=(Vector other)
 	{
 		return !(*this == other);
