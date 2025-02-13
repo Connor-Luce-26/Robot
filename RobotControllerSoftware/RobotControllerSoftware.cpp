6d36525ec6ccd1a7bcf56cca583f040eae098aba
Connor-Luce <lucerc@rose-hulman.edu>
2025-02-13 10:21:15 -0500

    Implement robot connection handling and command interface in RobotControllerSoftware

--- RobotControllerSoftware/RobotControllerSoftware.cpp
+++ RobotControllerSoftware/RobotControllerSoftware.cpp
@@ -3,12 +3,18 @@
 #include <iostream>
 #include "serialib.h"
 using namespace std;
+// Function declarations
 vector<int> getAvailableCOMPorts();
 int getTransmitterPort();
 int failedToGetTransmitterPort();
 serialib connectToTransmitter();
 serialib failedToConnectToTransmitter(serialib device, int transmitterPort);
 void sendCommand(serialib device, string command);
+void connectToRobot(serialib device);
+string receiveResponse(serialib device);
+void failedToConnectToRobot(serialib device);
+string getRobotCommand();
+// Function definitions
 vector<int> getAvailableCOMPorts()
 {
 	cout << "Start checking for available COM ports" << endl;
@@ -102,6 +108,26 @@ serialib connectToTransmitter()
 	}
 	return device;
 }
+void failedToConnectToRobot(serialib device)
+{
+	cout << "Failed to connect to robot" << endl;
+	cout << "Try again? (yes or no)" << endl;
+	string input;
+	cin >> input;
+	if (input == "yes")
+	{
+		connectToRobot(device);
+	} else if (input == "no")
+	{
+		cout << "Exiting program" << endl;
+		exit(0);
+	}
+	else
+	{
+		cout << "Invalid input" << endl;
+		failedToConnectToRobot(device);
+	}
+}
 void sendCommand(serialib device, string command)
 {
 	cout << "Sending command: " << command << endl;
@@ -114,8 +140,60 @@ void sendCommand(serialib device, string command)
 		cout << "Failed to send command" << endl;
 	}
 }
+string receiveResponse(serialib device)
+{
+	cout << "Receiving response" << endl;
+	char response[1024];
+	if (device.readString(response, '\n', 1000, 1000) == 1)
+	{
+		cout << "Response received: " << string(response) << endl;
+	}
+	else
+	{
+		cout << "Failed to receive response" << endl;
+	}
+	return string(response);
+}
+void connectToRobot(serialib device)
+{
+	cout << "Connecting to robot" << endl;
+	sendCommand(device, "connect");
+	string response = receiveResponse(device);
+	if (response == "connected")
+	{
+		cout << "Connected to robot" << endl;
+	}
+	else
+	{
+		failedToConnectToRobot(device);
+	}
+}
+string getRobotCommand(serialib device)
+{
+	cout << "Commands:" << endl;
+	cout << "sensors" << endl;
+	cout << "chassis move to \"x\" \"y\" \"z\"" << endl;
+	cout << "chassis turn to \"yaw\"" << endl;
+	cout << "arm move to \"x\" \"y\" \"z\"" << endl;
+	cout << "arm turn to \"roll\" \"pitch\" \"yaw\"" << endl;
+	cout << "gripper open" << endl;
+	cout << "gripper close" << endl;
+	cout << "close program" << endl;
+	cout << "Enter a command for the robot: ";
+	string command;
+	cin >> command;
+	sendCommand(device, command);
+	receiveResponse(device);
+	return command;
+
+}
 int main()
 {
 	serialib device = connectToTransmitter();
+	connectToRobot(device);
+	while (true)
+	{
+		string command = getRobotCommand();
+	}
 	return 0;
 }
