25b551b95e446567f45fd6d04c22389d20c6539a
Connor-Luce <lucerc@rose-hulman.edu>
2025-02-13 11:19:25 -0500

    Add RobotTransceiverSoftware for serial communication and remove obsolete RobotTransmitterSoftware

---
+++ RobotTransceiverSoftware/RobotTransceiverSoftware.ino
@@ -0,0 +1,31 @@
+#include <Arduino.h>
+#include <SoftwareSerial.h>
+#define TRANSCEIVER_TX_PIN 18
+#define TRANSCEIVER_RX_PIN 19
+#define SERIAL_BAUD 9600
+#define TRANSCEIVER_BAUD 9600
+#define TRANSCEIVER_M0_PIN 2
+#define TRANSCEIVER_M1_PIN 3
+#define TRANSCEIVER_AUX_PIN 4
+SoftwareSerial transceiverSerial(TRANSCEIVER_RX_PIN, TRANSCEIVER_TX_PIN);
+void setup() 
+{
+  Serial.begin(SERIAL_BAUD);
+  transceiverSerial.begin(TRANSCEIVER_BAUD);
+  pinMode(TRANSCEIVER_M0_PIN, OUTPUT);
+  pinMode(TRANSCEIVER_M1_PIN, OUTPUT);
+  pinMode(TRANSCEIVER_AUX_PIN, INPUT);
+  digitalWrite(TRANSCEIVER_M0_PIN, LOW);
+  digitalWrite(TRANSCEIVER_M1_PIN, LOW);
+}
+void loop()
+{
+  if (transceiverSerial.available()) {
+    char data = transceiverSerial.read();
+    Serial.print(data);
+  }
+  if (Serial.available()) {
+    char data = Serial.read();
+    transceiverSerial.print(data);
+  }
+}
